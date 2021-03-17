9fbd4b6fb019276f64b3db0617a5f92e86fb1387
BurningBlaze <pmdblaze@tutanota.com>
2021-03-17 17:54:25 -0400

    Change Eagle filename

--- Installer/ComponentDefinitions.cs
+++ Installer/ComponentDefinitions.cs
@@ -9,7 +9,7 @@ namespace Installer
     public class ComponentDefinitions
     {
         public static readonly ComponentDefinition Client = new ComponentDefinition(InstallationComponent.Client, "Client", "PMDShift-C.exe", "PMD Shift!", "Pokémon Mystery Dungeon: Shift!");
-        public static readonly ComponentDefinition Editor = new ComponentDefinition(InstallationComponent.Editor, "Editor", "Eagle.Editors.exe", "PMD Shift! - Editors", "Pokémon Mystery Dungeon: Shift! Editors");
+        public static readonly ComponentDefinition Editor = new ComponentDefinition(InstallationComponent.Editor, "Editor", "Eagle.exe", "PMD Shift! - Editors", "Pokémon Mystery Dungeon: Shift! Editors");
         public static readonly ComponentDefinition LegacyClient = new ComponentDefinition(InstallationComponent.LegacyClient, "LegacyClient", "PMDCP.exe", "PMD Shift! - Legacy", "Pokémon Mystery Dungeon: Shift! - Legacy Client");
     }
 }
